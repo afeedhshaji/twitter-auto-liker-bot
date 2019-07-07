2b66e277ed5b933ddb6dea6af64bf3a3bb10ec62
afeedh <afeedh_b170282cs@nitc.ac.in>
2019-07-07 12:51:01 +0530

    added login

--- app.py
+++ app.py
@@ -1,5 +1,35 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import keys
+from selenium.webdriver.common.keys import Keys
 
 import time
 
+
+class TwitterBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+        bot = self.bot
+        bot.get('https://twitter.com/')
+        time.sleep(3)  # wait for the page to load it's contents
+        email = bot.find_element_by_class_name('email-input')
+        password = bot.find_element_by_name('session[password]')
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(3)
+
+    def like_tweet(self, hashtag):
+        bot = self.bot
+        bot.get('https://twitter.com/search?q='+hashtag+'&src=typd')
+        time.sleep(3)
+
+
+ed = TwitterBot('__afeedh__', 'Shady15')
+
+ed.login()
+ed.like_tweet('webdevelopment')
